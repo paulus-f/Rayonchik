3bca9fe9952125ee8f089b05b120fd0914479dac
Pavel <fedorovich2012bel@gmail.com>
2019-03-17 11:06:04 +0300

    fix bug

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -3,6 +3,7 @@ class Suggestion < ApplicationRecord
   belongs_to :region
   belongs_to :user
   has_many :votes
+  belongs_to :region
   scope :approving, -> { where(approve: true) }
   def can_vote(current_user)
     votes.where(user_id: current_user.id).nil? == [] ? true : false
